2f131844b37f48a8d5da617292188731687437c5
zengyr <zengyr7@midea.com>
2024-11-12 11:16:30 +0800

    feat: read from oss and chat

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::collections::HashMap;
+use futures::StreamExt;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use tokio;
-use futures::StreamExt;
-use serde_json::json;
 
 #[derive(Serialize)]
 struct ChatBody {
@@ -41,9 +39,39 @@ async fn main() -> Result<(), reqwest::Error>{
     println!("Hello, world!");
     // 创建一个Client 实例
     let client = Client::new();
+
+    // 从oss拉取文件，先尝试用http链接，后续TODO：用s3的方式；
+    // let file_path_url = "https://oss-cn-foshan-2.biubiu.com/service-log/%E9%97%AE%E9%A2%98%E7%AD%94%E7%96%91.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=yt8fbg0lugdrtrw5wbyfmfyl%2F20241112%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241112T022436Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&response-content-type=application%2Foctet-stream&X-Amz-Signature=c6eff8f8effb8fc1564e2d5b93bde453f65aa66b7824889b7a7ff4cd47c4d082";
+    let file_path_url = "your_oss_or_other_remote_file_path";
+    let result = reqwest::get(file_path_url).await;
+    let mut content = "".to_string();
+    match result {
+        Ok(response) => {
+            if response.status().is_success() {
+                content = response.text().await.unwrap();
+            } else {
+                println!("error: {}", response.status());
+                assert!(false, "error: {}", response.status());
+            }
+        }
+        Err(e) => {
+            println!("error: {}", e);
+        }
+    }
+
+    let mut prompt = format!("根据上下文回答问题，请用简体中文简洁地回答。
+    上下文:
+    \"\"\"
+    {}
+    \"\"\"
+    问题: {}
+    ", content, "mgp-client 2.x版本读取discovery配置出错，可能的原因是什么");
+
+    // "你的老板经营一家羽毛球馆，你是这家羽毛球馆的智能AI。球馆有7片场地，每天闲时0-17点场地费用50一小时，忙时18-24点费用60一小时。其他经营业务有：羽毛球教学、相关器具出售、羽毛球比赛承办。如果有不懂的地方请联系老板，老板的电话是1234567，姓牛。接下来的提问在我没有具体声明之前，请按照这个背景给出答案。如果有不确定的点，请避免给出一个模糊的或者编造的答案，比如你需要避免编造课程价格和场地费用价格或者器械价格，同时因为你是个智能AI，因此你也无法自行联系场馆，如有需要请告知提问者联系前述的场馆老板牛先生。".to_string()
+
     let mut body = ChatBody {
         model: "llama3.1".to_string(),
-        prompt: "你的老板经营一家羽毛球馆，你是这家羽毛球馆的智能AI。球馆有7片场地，每天闲时0-17点场地费用50一小时，忙时18-24点费用60一小时。其他经营业务有：羽毛球教学、相关器具出售、羽毛球比赛承办。如果有不懂的地方请联系老板，老板的电话是1234567，姓牛。接下来的提问在我没有具体声明之前，请按照这个背景给出答案。如果有不确定的点，请避免给出一个模糊的或者编造的答案，比如你需要避免编造课程价格和场地费用价格或者器械价格，同时因为你是个智能AI，因此你也无法自行联系场馆，如有需要请告知提问者联系前述的场馆老板牛先生。".to_string(),
+        prompt: prompt,
         stream: false,
         options: Options {temperature: 0.8} // temperature的默认值，0.8,值越大，回答越creative
     };
@@ -67,8 +95,16 @@ async fn main() -> Result<(), reqwest::Error>{
     while let Some(item) = stream.next().await {
         let item = item?;
         // 拼装和返回
-        let temp_res: ChatResponse = serde_json::from_slice(&item).unwrap();
-        print!("{:?}", temp_res.response.unwrap().replace("\"", ""));
+        let temp_res:Result<ChatResponse, serde_json::Error> = serde_json::from_slice(&item);
+        match temp_res {
+            Ok(chatRes) => {
+                print!("{}", chatRes.response.unwrap());
+            }
+            Err(e) => {
+            }
+        }
+        
+        // print!("{}", temp_res.response.unwrap());
     }
 
     Ok(())
